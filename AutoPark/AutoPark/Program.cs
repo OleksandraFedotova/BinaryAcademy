6b4fdc21231f03e24d8c3d98162e95252eac07c4
Alexandra Fedotova <feyasasha19111996@gmail.com>
2016-05-25 00:52:17 +0300

    [test-proj] added class and Method, call it in Main

--- AutoPark/AutoPark/Program.cs
+++ AutoPark/AutoPark/Program.cs
@@ -10,6 +10,27 @@ namespace AutoPark
     {
         static void Main(string[] args)
         {
+            Auto car = new Auto(1223, "Ford", 1243581);
+            string a = Auto.GetInfo(car);
+        }
+    }
+
+    class Auto
+    {
+        private int number;
+        public string model;
+        protected int price;
+
+        public Auto(int number, string model, int price)
+        {
+            this.number = number;
+            this.model = model;
+            this.price = price;
+        }
+
+        public static string GetInfo(Auto a)
+        {
+            return "model" + a.model + ",id=" + a.number + " costs" + a.price;
         }
     }
 }
